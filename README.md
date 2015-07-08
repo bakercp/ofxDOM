708e366509cdfdabce3b1e2e3800c0d8412ecabb
Christopher Baker <me@christopherbaker.net>
2015-07-07 22:05:55 -0500

    Initial updates.

---
+++ README.md
@@ -0,0 +1,7 @@
+#ofxDOM
+
+A DOM Level 3 Events / UI Events implementation for openFrameworks.
+
+http://www.w3.org/TR/DOM-Level-3-Events/
+
+Requires openFrameworks 0.9.0+.
---
+++ addon_config.mk
@@ -0,0 +1,9 @@
+meta:
+	ADDON_NAME = ofxDOM
+	ADDON_DESCRIPTION = A DOM Level 3 Events implementation for openFrameworks.
+	ADDON_AUTHOR = bakercp
+	ADDON_TAGS = "UI" "events" "DOM"
+	ADDON_URL = http://github.com/bakercp/ofxDOM
+
+common:
+	ADDON_DEPENDENCIES = ofxPointer
