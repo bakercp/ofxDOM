6bbd38f07716dca0ec69cac647f6939b3132a9db
Christopher Baker <me@christopherbaker.net>
2016-01-13 16:20:30 -0600

    Update documentation.

--- libs/ofxDOM/include/ofx/DOM/CapturedPointer.h
+++ libs/ofxDOM/include/ofx/DOM/CapturedPointer.h
@@ -37,29 +37,66 @@ namespace DOM {
 class CapturedPointer
 {
 public:
+    /// \brief Create a CapturedPointer with the given id.
+    /// \param id The id of the captured pointer.
     CapturedPointer(std::size_t id);
+
+    /// \brief Destroy the captured pointer.
     ~CapturedPointer();
 
+    /// \brief Update a given Captured Pointer from an Element and PointerEvent.
+    ///
+    /// This method updates offset, position, velocity, lastUpdate and other
+    /// parameters of the CapturedPointer.
+    ///
+    /// \param element The element that is currently handling the Pointer.
+    /// \param e The PointerEvent used to update the CapturedPointer's data.
     void update(Element* element, const PointerEvent& e);
 
+    /// \returns the captured pointer's id.
 	std::size_t id() const;
+
+    /// \returns the start position of the captured pointer.
 	const Position& start() const;
-	const Position& offset() const;
+
+    /// \returns the start()-position() of the captured pointer.
+    const Position& offset() const;
+
+    /// \returns the current position of the captured pointer.
 	const Position& position() const;
+
+    /// \returns the velocity of the captured pointer (lastPosition() - position()).
 	const Position& velocity() const;
+
+    /// \returns the last update time.
 	uint64_t lastUpdate() const;
+
+    /// \returns the initial timestamp when the pointer was captured.
 	uint64_t timestamp() const;
 
 	friend std::ostream& operator << (std::ostream& os,
-									  const CapturedPointer& vec);
+									  const CapturedPointer& pointer);
 
 private:
+    /// \brief The captured pointer's id.
     std::size_t _id;
+
+    /// \brief The captured pointer's start position.
     Position _start;
+
+    /// \brief The captured pointer's current offset from its start position.
     Position _offset;
+
+    /// \brief The captured pointer's current position.
     Position _position;
+
+    /// \brief The captured pointer's current velocity.
     Position _velocity;
+
+    /// \brief The timestamp of the last update.
     uint64_t _lastUpdate;
+
+    /// \brief The timestamp when the pointer was captured.
     uint64_t _timestamp;
 
 };
