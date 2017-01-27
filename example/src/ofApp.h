3399dbf36b8197afdbd929cb05a0f37a9c6bf0ab
Christopher Baker <me@christopherbaker.net>
2017-01-27 00:50:02 -0600

    Add  empty example.

---
+++ example/src/ofApp.h
@@ -0,0 +1,23 @@
+//
+// Copyright (c) 2009 Christopher Baker <https://christopherbaker.net>
+//
+// SPDX-License-Identifier:	MIT
+//
+
+
+#pragma once
+
+
+#include "ofMain.h"
+#include "ofxDOM.h"
+
+
+class ofApp: public ofBaseApp
+{
+public:
+    void setup();
+    void draw();
+
+    ofxDOM::Document document;
+
+};
